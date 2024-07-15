eb808076029391d57b16f1a66e7321da49408859
kou12345 <hi.0412.ko.2002@gmail.com>
2024-07-16 01:11:35 +0900

    fix

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,20 +1,31 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { useState } from "react";
+import { TaskFormDialog } from "./TaskFormDialog";
 
 export const Header = () => {
+  const [date, setDate] = useState<Date>();
   return (
     <>
-      <h1 className="col-start-3 c</div>ol-span-2 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      <h1 className="c</div>ol-span-2 col-start-3 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         <Link href="/">Do</Link>
       </h1>
 
-      <div className="col-end-10 col-span-3 flex items-center space-x-2">
+      <div className="col-span-3 col-end-10 flex items-center space-x-2">
         <Input type="text" />
         <Button>Search</Button>
       </div>
-      <div className="col-end-11 col-span-1 flex items-center">
-        <Button>Create</Button>
+      <div className="col-span-1 col-end-11 flex items-center">
+        <TaskFormDialog
+          mode="create"
+          title=""
+          description=""
+          dueDate={date}
+          setDueDate={setDate}
+        />
       </div>
     </>
   );
